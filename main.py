b2e4d29d3df9be7363eaff33d2de64359b0fe886
Vankad24 <vankad24@mail.ru>
2023-07-03 14:18:58 +0800

    Use the ionoplot module

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from datetime import datetime
+from ionoplot import MapType, plot_maps, plot_distance_time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+EPICENTERS = {'01:17': {'lat': 37.220,
+                        'lon': 37.019,
+                        'time': datetime(2023, 2, 6, 1, 17, 34)},
+              '10:24': {'lat': 38.016,
+                        'lon': 37.206,
+                        'time': datetime(2023, 2, 6, 10, 24, 50)}
+             }
 
+times = [datetime(2023, 2, 6, 10, 25),
+                 datetime(2023, 2, 6, 10, 40),
+                 datetime(2023, 2, 6, 10, 45)]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plot_maps(["notebook/roti_10_24.h5", "notebook/tnpgn_roti_10_24.h5"], MapType.ROTI, times, EPICENTERS['10:24'], save_path="./out/time3.png")
+plot_distance_time("notebook/dtec_10_20_10_24.h5", MapType.TEC_10_20, EPICENTERS['10:24'], save_path="./out/time2.png")
